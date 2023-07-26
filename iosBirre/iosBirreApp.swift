bfb13919f96bcae6b5d76b357f0a928167596956
giogiocamp96 <giorgia.campanile06@gmail.com>
2023-07-26 12:30:37 +0200

    Initial Commit

---
+++ iosBirre/iosBirreApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosBirreApp.swift
+//  iosBirre
+//
+//  Created by Giorgia Campanile on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iosBirreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
